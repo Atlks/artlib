2fcab4681492280a85801471e81b7599b6fdb79d
ati <ati@atiland.com>
2020-10-28 23:58:52 +0800

    ....

---
+++ 纹章学/纹章学.mm
@@ -0,0 +1,47 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1599220879754" ID="5f522bc120a68c658e4ba2f0" MODIFIED="1599220902378" TEXT="&#x7eb9;&#x7ae0;&#x5b66;">
+<edge COLOR="#b4b4b4"/>
+<font NAME="Dialog" SIZE="17"/>
+<node CREATED="1599220879755" ID="465b332baa72460bd5cdc20e" MODIFIED="1599220879755" POSITION="right" TEXT="&#x56fd;&#x65d7;&#x8bbe;&#x8ba1;">
+<edge COLOR="#e68782"/>
+<font NAME="Dialog" SIZE="15"/>
+<node CREATED="1599220879755" ID="342f49ff2f0c3c7d52320c56" MODIFIED="1599220879755" TEXT="">
+<edge COLOR="#e88f8c"/>
+<font NAME="Dialog" SIZE="13"/>
+</node>
+</node>
+<node CREATED="1599220879755" ID="fb98ba23254b557499c03334" MODIFIED="1599220879755" POSITION="left" TEXT="logo">
+<edge COLOR="#e096e9"/>
+<font NAME="Dialog" SIZE="15"/>
+</node>
+<node CREATED="1599220879755" ID="c5264c8568fdb2323c5f7e1c" MODIFIED="1599220879755" POSITION="left" TEXT="&#x7ef6;&#x5e26;">
+<edge COLOR="#7aa3e5"/>
+<font NAME="Dialog" SIZE="15"/>
+</node>
+<node CREATED="1599220879755" ID="8ebeaa1142a2a93398d4efba" MODIFIED="1599220879755" POSITION="left" TEXT="&#x5fbd;&#x7ae0;">
+<edge COLOR="#988ee3"/>
+<font NAME="Dialog" SIZE="15"/>
+<node CREATED="1599220879755" ID="6599e3231a67703957013d19" MODIFIED="1599220879755" TEXT="&#x52cb;&#x7ae0;">
+<edge COLOR="#9e96e7"/>
+<font NAME="Dialog" SIZE="13"/>
+</node>
+</node>
+<node CREATED="1599220879755" ID="52663f656fb22ee9c6775d56" MODIFIED="1599220879755" POSITION="right" TEXT="other">
+<edge COLOR="#67d7c4"/>
+<font NAME="Dialog" SIZE="15"/>
+<node CREATED="1599220879755" ID="5b7e9b5eb801422cf615426d" MODIFIED="1599220879755" TEXT="&#x5bb6;&#x6587;">
+<edge COLOR="#60d4be"/>
+<font NAME="Dialog" SIZE="13"/>
+</node>
+<node CREATED="1599220879755" ID="6188cc32a32f5c8d43eb795b" MODIFIED="1599220879755" TEXT="&#x5546;&#x6807;">
+<edge COLOR="#67d9c4"/>
+<font NAME="Dialog" SIZE="13"/>
+</node>
+</node>
+<node CREATED="1599220879756" ID="6810a1b8e66539536ed96a29" MODIFIED="1599220879756" POSITION="left" TEXT="coggle mindmap">
+<edge COLOR="#9ed56b"/>
+<font NAME="Dialog" SIZE="15"/>
+</node>
+</node>
+</map>
